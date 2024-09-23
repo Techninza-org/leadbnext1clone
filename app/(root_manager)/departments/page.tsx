785b69236a2e7b519f434c0984adff5b809579f0
Kashish <kashish.biserwal@protonmail.com>
2024-09-23 13:59:23 +0530

    page fix

--- app/(root_manager)/departments/page.tsx
+++ app/(root_manager)/departments/page.tsx
@@ -1,15 +1,9 @@
-'use client'
-import CompanyDepartmentsRoot from '@/components/User/Admin/company-departments-root'
-import { userAtom } from '@/lib/atom/userAtom'
-import { useAtomValue } from 'jotai'
 import React from 'react'
+import RootDepts from '@/components/User/Admin/root-depts'
 
 const page = () => {
-    const user = useAtomValue(userAtom)
   return (
-    <div>
-        <CompanyDepartmentsRoot id={String(user?.companyId)} />
-    </div>
+    <RootDepts />
   )
 }
 
