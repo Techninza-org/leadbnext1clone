5f4b2a80496b5f2951aa6206d42996dece65a090
aloksharma10 <aloksharma10969@gmail.com>
2024-10-14 12:36:26 +0530

    FormPage fix

--- app/(root_manager)/departments/form/[deptName]/[deptId]/[companyId]/page.tsx
+++ app/(root_manager)/departments/form/[deptName]/[deptId]/[companyId]/page.tsx
@@ -1,5 +1,5 @@
 import UpdateDepartmentFieldsModal from "@/components/dynamic/create-department-modal";
 
 export default function FormPage({ params }: { params: { deptName: string, deptId: string, companyId: string } }) {
-    return (<UpdateDepartmentFieldsModal deptName={params.deptName} deptId={params.deptId} companyId={params.companyId} />);
+    return (<UpdateDepartmentFieldsModal deptName={params.deptName} deptId={params.deptId} />);
 }
